4203c74d8c724b0a7b2cf62c4989a228683fbb60
Ruben Cancho <canchete@gmail.com>
2022-02-06 16:15:50 +0100

    Change background every minute

--- ksetwallpaper.sh
+++ ksetwallpaper.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-sleep 15
-echo "HOLA" > /tmp/ksetwallpaper.log
 file="/usr/share/wallpapers/Altai/contents/images/1080x1920.png"
-/usr/local/bin/ksetwallpaper.py $file
+
+# Esperamos a que plasmashell esté listo
+sleep 15
+
+# Para evitar tentaciones...
+for i in {1..50}
+do
+  /usr/local/bin/ksetwallpaper.py $file
+  sleep 1m
+done
