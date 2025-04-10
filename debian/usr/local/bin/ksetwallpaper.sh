fa59af3d8dce5a97bbab5bf938a451e9efb1e144
Rubén Cancho <304053+Canx@users.noreply.github.com>
2025-04-10 10:07:51 +0200

    Cambiada resolución

--- debian/usr/local/bin/ksetwallpaper.sh
+++ debian/usr/local/bin/ksetwallpaper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-file="/usr/share/wallpapers/reglas_centro/contents/images/5000x2813.png"
+file="/usr/share/wallpapers/reglas_centro/contents/images/normas_1366.png"
 
 # Esperamos a que plasmashell esté listo
 sleep 15
